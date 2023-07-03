a5b0df9f403e7f539b37eec8cc337842db9515a0
Harrier <przelajowiec@gmail.com>
2023-07-03 12:07:19 +0200

    2 commit

--- src/main/java/pl/harrier/Main.java
+++ src/main/java/pl/harrier/Main.java
@@ -6,6 +6,8 @@ public class Main{
         Sudoku.board[3][3] = 7;
         Sudoku.board[5][4] = 1;
         Sudoku.board[0][8] = 9;
+        Sudoku.board[3][1] = 4;
+        Sudoku.board[4][8] = 5;
         Sudoku.show();
     }
 }
